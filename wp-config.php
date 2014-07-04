b51e8a6534fb3eebbdfb30a0d18a8177e856224b
Mikdiet <mikdiet@gmail.com>
2014-07-04 11:13:35 +0300

    local setup

--- wp-config.php
+++ wp-config.php
@@ -16,13 +16,13 @@
 
 // ** Параметры MySQL: Эту информацию можно получить у вашего хостинг-провайдера ** //
 /** Имя базы данных для WordPress */
-define('DB_NAME', 'database_name_here');
+define('DB_NAME', 'wp_demo');
 
 /** Имя пользователя MySQL */
-define('DB_USER', 'username_here');
+define('DB_USER', 'wp_demo');
 
 /** Пароль к базе данных MySQL */
-define('DB_PASSWORD', 'password_here');
+define('DB_PASSWORD', 'wp_demo');
 
 /** Имя сервера MySQL */
 define('DB_HOST', 'localhost');
